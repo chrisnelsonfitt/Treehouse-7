38833287611267025738e27f407e9e773142fe23
Chris Nelson <c.m.nelson.fitt@gmail.com>
2016-01-23 01:02:24 -0700

    Finished
    I didn’t use Javascript I used Jq
    Also  I didn’t follow the layout plan perfectly
    And I didn’t change the buttons.
    Also it isn’t liking my 100% width in html

--- js/app.js
+++ js/app.js
@@ -1,77 +1,47 @@
-//hide buttons
-
-
-
 var video = document.getElementsByTagName("video")[0];
 
+//Hide and Show Buttons
+$('.mejs-button, .mejs-currenttime, .mejs-duration , .mejs-time span').css("display", "none");
+$('.mejs-skin, .mejs-time-rail').mouseenter(function () {$('.mejs-button , .mejs-currenttime , .mejs-time span').css("display", "block"); });
+$('.mejs-skin').mouseleave(function () {$('.mejs-button , .mejs-currenttime, .mejs-time span').css("display", "none"); });
 
-$("p , button , .mejs-time-rail ").click(
-    function(){
-        $('p').removeClass("hlight");
-        
-});
+//Remove script highlight on click
+$("p , button , .mejs-time-rail ").click(function () { $('p').removeClass("hlight"); });
 
-video.addEventListener("timeupdate", function() {
-    if (video.currentTime < 7) {
-        $("#1").addClass("hlight");
-        }
-    else if (video.currentTime <= 17) {
+//Highlight text based on position of video
+video.addEventListener("timeupdate", function () {
+    if (video.currentTime < 7.53) {$("#1").addClass("hlight"); } else if (video.currentTime <= 17.9) {
         $("#2").addClass("hlight");
         $("#1").removeClass("hlight");
-    }
-    else if (video.currentTime <= 32) {
-         $("#3").addClass("hlight");
-         $("#2").removeClass("hlight");
-    }
-    else if (video.currentTime <= 42) {
-         $("#4").addClass("hlight");
-         $("#3").removeClass("hlight");
-    }
-    else if (video.currentTime <= 53) {
-         $("#5").addClass("hlight");
-         $("#4").removeClass("hlight");
-    }
-    else if (video.currentTime <= 60) {
-          $("#6").addClass("hlight");
-          $("#5").removeClass("hlight");
-    }
+    } else if (video.currentTime <= 32) { $("#3").addClass("hlight");
+         $("#2").removeClass("hlight"); } else if (video.currentTime <= 42) { $("#4").addClass("hlight");
+         $("#3").removeClass("hlight"); } else if (video.currentTime <= 53) { $("#5").addClass("hlight");
+         $("#4").removeClass("hlight"); } else if (video.currentTime <= 60) { $("#6").addClass("hlight");
+          $("#5").removeClass("hlight"); }
 });
 
-// link script and video feed times
-    //link time with paragraph
-    //scroll up to video on click
-    // run script highlight
- 
-
-$("#1").click(function(){
+//link video with paragraph   
+$("#1").click(function () {
     video.currentTime = 0;
     video.play();
-    
 });
-$("#2").click(function(){
+$("#2").click(function () {
     video.currentTime = 8;
     video.play();
-    
 });
-$("#3").click(function(){
+$("#3").click(function () {
     video.currentTime = 17;
     video.play();
-    
 });
-$("#4").click(function(){
+$("#4").click(function () {
     video.currentTime = 32;
-    video.play
-    
+    video.play();
 });
-$("#5").click(function(){
+$("#5").click(function () {
     video.currentTime = 42;
     video.play();
-    
 });
-$("#6").click(function(){
+$("#6").click(function () {
     video.currentTime = 53;
     video.play();
-    
 });
-
-
